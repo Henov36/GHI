87559fae28cc6dc72aadf69ce960d09846baea41
Henov36 <fiz.ryk.5656@gmail.com>
2026-03-02 21:16:31 +0300

    secondary design

--- js/mail.js
+++ js/mail.js
@@ -5,6 +5,11 @@ const swiper = new Swiper(".mySwiper", {
 	slidesPerGroup: 2,
 	spaceBetween: 40, // Расстояние между ними (в пикселях)
 	loop: true, // Бесконечная прокрутка
+	autoplay: {
+		delay: 2000, // Пауза между переключениями (в миллисекундах, 3000 = 3 сек)
+		disableOnInteraction: false, // Продолжать крутить, даже если пользователь кликал на слайдер
+		pauseOnMouseEnter: true, // Останавливать прокрутку при наведении мышки
+	},
 	navigation: {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev",
@@ -15,7 +20,7 @@ const swiper = new Swiper(".mySwiper", {
 	},
 	// Добавим адаптивность: на мобилках 1 фото, на компах 2
 	breakpoints: {
-		320: { slidesPerView: 1 },
-		768: { slidesPerView: 2 },
+		320: { slidesPerView: 1, slidesPerGroup: 1 },
+		768: { slidesPerView: 2, slidesPerGroup: 2 },
 	},
 });
